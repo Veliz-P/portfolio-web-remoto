c09b6502b2b8eaa3b78ce2f8d0633fc1f26b4fd1
Veliz <paucarcarlos2108@gmail.com>
2025-03-27 17:44:18 -0500

    feat(app): Crear vista para contenido de proyecto cavamym

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cavamym/", views.project_cavamym_view, name="cavamym")
 ]
